920e27cee155f84a1c6de9b0421fbbce9bc26a54
Asif Akbar <786asifakbar53@gmail.com>
2025-10-26 00:19:13 +0500

    jokes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
+const [jokes , setJokes ] = useState([])
 
   return (
     <>
       <h1> chai aur code </h1>
-        
+      <p>Jokes : {jokes.length}</p>
+
+
+
+{
+  jokes.map((jokeObj) => (
+    <div key={jokeObj.id}>
+      <h3>Setup: {jokeObj.setup}</h3>
+      <p>Punchline: {jokeObj.punchline}</p>
+      <hr />
+    </div>
+  ))
+}
     </>
   )
 }
