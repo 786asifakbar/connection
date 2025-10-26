38495e917623f724e94f9ecf3e83e09e16ff43e9
Asif Akbar <786asifakbar53@gmail.com>
2025-10-26 11:15:27 +0500

    frontend and backend connected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,30 @@
+import { useEffect, useState } from 'react'
 import './App.css'
-
+import axios from 'axios'
 
 function App() {
+
 const [jokes , setJokes ] = useState([]) // create empty array to store jokes.
+
+useEffect(()=>{
+  axios.get(`/api/jokes`)
+  .then((response)=>{
+     setJokes(response.data);
+  }).catch((error)=>{
+    console.log(error);
+  })
+})
 
   return (
     <>
       
       <h1> chai aur code </h1>
-      <p>Jokes : {jokes.length} </p> // value of jokes length 
+      <p>Jokes : {jokes.length} </p> {/* display number of jokes */} 
 
       {
         jokes.map((joke , index) => (
-          <div key={index,id}>
-            <p>{joke.title}</p>
+          <div key={index.id}>
+            <h3>{joke.title}</h3>
             <p>{joke.content}</p>
           </div>
         ))
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server : {
+    proxy : {
+           '/api' :  'http://localhost:3000'    // is code likhny ka maqsad hai k jb bhi apllication kise bhi port
+                                                // par open ho to usko is port k sath replace kar de
+    },
+  },
+
   plugins: [react()],
 })
